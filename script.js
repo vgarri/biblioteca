32758f608e40a3aa032728bfd1c87aa0f89a781f
Victor Garritano <victor@getupgraded.com>
2024-10-30 00:16:30 +0100

    FASE 1 FALTA SPINNER Y GO BACK

--- script.js
+++ script.js
@@ -4,28 +4,91 @@ const apiKey = "6NXaahrsbBQTQ7wlGBAOKlrNMnYoacxd";
 //https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=6NXaahrsbBQTQ7wlGBAOKlrNMnYoacxd
 
 
+
 async function obtainData() {
     let response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${apiKey}`);
     let data = await response.json();
-    let resultados = data.results;
-    let name;
-    let dateOld;
-    let dateLast;
-    let dateUpdate;
-    let link;
-    resultados.forEach((resultado) => {
-        name = resultado.list_name;
-        dateOld = resultado.oldest_published_date;
-        dateLast = resultado.newest_published_date;
+
+    return data.results;
+}
+obtainData()
+// Función para cargar todas las listas en el DOM
+async function createCardsDom() {
+    let datos = await obtainData();
+    let section = document.body.querySelector("#listsContainer")
+
+    datos.forEach(list => {
+
+        section.innerHTML += `
+        <article>
+        <div class='tarjetalista'>
+        <h3>${list.list_name}</h3>
+        <p>Oldest: ${list.oldest_published_date}</p>
+        <p>Newest: ${list.newest_published_date}</p>
+        <p>Updated: ${list.updated.toLowerCase()}</p>
+        <button class ='cargarLista' id="${list.list_name}">
+        <span> READ MORE </span>
+        </div>
+        
+        </button>
+        </article>`
+            ;
+
+    })
+    document.querySelectorAll(".cargarLista").forEach(button => {
+        button.addEventListener("click", async function () {
+            let id = this.getAttribute("id");
+            let datosLista = await fetch(`https://api.nytimes.com/svc/books/v3/lists/${id}.json?api-key=${apiKey}`)
+            let data = await datosLista.json();
+
+            section.innerHTML = "";//para limpiar el dom
+            //mismo procedimiento anterior:
+            data.results.books.forEach(book => {
+                section.innerHTML += `
+                
+                
+                <div class='tarjetalibro'>
+                <h4>#${book.rank} ${book.title}</h4>
+                <div class="caratula">
+                <img src=${book.book_image}></img>
+                </div>
+                <p>Weeks on the list: ${book.weeks_on_list}</p>
+                <p>Description: ${book.description}</p>
+                
+                <button class ='comprarAmazon' onclick=window.location.href="${book.amazon_product_url}" target="_blank">
+                BUY NOW
+                
+                </button>
+                `
+                
+            })
         
 
 
-    });
 
 
-    
+        })
+    })
+}
+createCardsDom();
+
+async function generarInfoLista() {
+    let datos = await fetch();
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const spinner = async () => {
 //     return new Promise((resolve, reject) => {
@@ -35,11 +98,15 @@ async function obtainData() {
 //   });   
 // }
 
-obtainData();
 
-async function createCards(){
-    
+// async function createCards(){
+
+//    let ul = document.createElement("ul");
+//     let li = document.createElement("li");
+//     li.innerHTML = `<p></p>`
+//     ul.appendChild(li)
+//   document.body.appendChild(ul)
 
 
-
-}
+// }
+// createCards();
